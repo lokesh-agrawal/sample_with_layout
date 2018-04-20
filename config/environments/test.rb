ac43cdf8747a7e5512c6728b79763dd92f603f28
Lokesh <lokesh.agrawal@freshworks.com>
2018-04-20 18:32:26 +0530

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,6 +5,7 @@ SampleApp::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
+
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
@@ -34,4 +35,9 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
